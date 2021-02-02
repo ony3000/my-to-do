fe33d5fc89975f4304daacc55cbd35495fb5ebb2
Hyeonjong <nianelo4@gmail.com>
2021-02-03 02:17:48 +0900

    Add links of to-do pages

--- components/layouts/AppLeftColumn.js
+++ components/layouts/AppLeftColumn.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import Link from 'next/link';
+import { withRouter } from 'next/router';
 
 class AppLeftColumn extends React.Component {
   render() {
-    return <div>App Left Column</div>;
+    const { router } = this.props;
+    const anchors = [
+      {
+        key: 'myday',
+        text: '오늘 할 일',
+        href: '/tasks/myday',
+      },
+      {
+        key: 'important',
+        text: '중요',
+        href: '/tasks/important',
+      },
+      {
+        key: 'planned',
+        text: '계획된 일정',
+        href: '/tasks/planned',
+      },
+      {
+        key: 'inbox',
+        text: 'Tasks',
+        href: '/tasks/inbox',
+        hrefAliases: [
+          '/tasks',
+        ],
+      },
+    ];
+
+    return (
+      <div>
+        <ul>
+          {anchors.map((anchorItem) => (
+            <li
+              key={anchorItem.key}
+              className={router.pathname === anchorItem.href || anchorItem.hrefAliases?.includes(router.pathname) ? 'font-bold' : ''}
+            >
+              <Link href={anchorItem.href}>
+                <a className="block">{anchorItem.text}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
-export default AppLeftColumn;
+export default withRouter(AppLeftColumn);
