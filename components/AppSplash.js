b777f46f4a3625ffde017aae8214767a68d1408d
Hyeonjong <nianelo4@gmail.com>
2021-02-18 00:41:20 +0900

    Separate styles defined inside script into files

--- components/AppSplash.js
+++ components/AppSplash.js
@@ -1,41 +1,18 @@
 import React from 'react';
-import resolveConfig from 'tailwindcss/resolveConfig';
-import tailwindConfig from '../tailwind.config';
+import classNames from 'classnames/bind';
+import styles from './AppSplash.module.scss';
 
-const fullConfig = resolveConfig(tailwindConfig);
+const cx = classNames.bind(styles);
 
 class AppSplash extends React.Component {
   render() {
     return (
-      <main>
-        <img className="icon" src="/images/todo_check.png" alt="" />
+      <main className={cx('main')}>
+        <img className={cx('icon')} src="/images/todo_check.png" alt="" />
 
-        <span className="spinner animate-spin">
+        <span className={cx('spinner', 'animate-spin')}>
           <span className="sr-only">Loading...</span>
         </span>
-
-        <style jsx>{`
-          main {
-            padding: 8.75rem 0 5rem;
-            flex: 1;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-          }
-
-          .icon {
-            margin: auto 0;
-          }
-
-          .spinner {
-            width: 60px;
-            height: 60px;
-            border: 2px solid transparent;
-            border-radius: 50%;
-            border-top-color: ${fullConfig.theme.colors.blue[500]};
-          }
-        `}</style>
       </main>
     );
   }
