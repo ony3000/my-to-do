fe2a2cf67a4e2b0026a4b97f5642aa335a0eab1b
Hyeonjong <nianelo4@gmail.com>
2021-06-03 01:07:09 +0900

    Remove dummy tasks

--- store/todoSlice.js
+++ store/todoSlice.js
@@ -1,7 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { v4 as uuid } from 'uuid';
 import merge from 'lodash.merge';
-import dayjs from '@/plugins/dayjs';
 
 export const CHANGE_THEME = 'CHANGE_THEME';
 export const TOGGLE_COMPLETED_ITEMS = 'TOGGLE_COMPLETED_ITEMS';
@@ -118,7 +117,7 @@ const loadState = () => JSON.parse(localStorage.getItem('cloneCoding:my-to-do'))
 export const launchApp = createAsyncThunk('todo/launchApp', async () => {
   const promise = new Promise(async (resolve) => {
     // API 호출로 데이터를 가져온다고 가정했을 때, 요청 완료되는 시간이 고정되어있지 않음을 나타냄
-    const delay = 500 + Math.floor(Math.random() * 100);
+    const delay = 350 + Math.floor(Math.random() * 150);
 
     const combinedState = merge({}, initialState, loadState(), {
       isActiveListOption: false,
@@ -133,44 +132,6 @@ export const launchApp = createAsyncThunk('todo/launchApp', async () => {
       deadlineCalendarPosition: {},
       focusedTaskId: null,
     });
-
-    if (combinedState.todoItems.length === 0) {
-      const getRandomInt = (min, max) => (min + Math.floor(Math.random() * (max + 1)));
-
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-      const dummyItems = await response.json();
-
-      const now = dayjs();
-      const midnightToday = now.startOf('day');
-
-      combinedState.todoItems = dummyItems.slice(0, 20).map(({ id, title, completed }, itemIndex) => {
-        const createdAt = Number(now.format('x')) - (20 - id) * 1000;
-
-        return {
-          id: uuid(),
-          title,
-          isComplete: completed,
-          subSteps: Array(getRandomInt(0, 3)).fill(null).map((_, stepIndex) => ({
-            id: uuid(),
-            title: dummyItems[itemIndex * 10 + stepIndex].title,
-            isComplete: dummyItems[itemIndex * 10 + stepIndex].completed,
-            createdAt: createdAt + (stepIndex + 1) * 1000,
-          })),
-          isImportant: Boolean(getRandomInt(0, 1)),
-          isMarkedAsTodayTask: Boolean(getRandomInt(0, 1)),
-          deadline: (
-            Boolean(getRandomInt(0, 1))
-              ? Number(midnightToday.add(getRandomInt(-3, 7), 'day').format('x')) - 1
-              : null
-          ),
-          memo: Boolean(getRandomInt(0, 1)) ? dummyItems[itemIndex * 10 + 5].title : '',
-          createdAt,
-          completedAt: completed ? createdAt : null,
-          markedAsImportantAt: createdAt,
-          markedAsTodayTaskAt: createdAt,
-        };
-      });
-    }
 
     setTimeout(() => {
       resolve({
