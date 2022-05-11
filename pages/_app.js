31eed925f92d14a455c3bf6266d5d850d6838e0b
Hyeonjong <nianelo4@gmail.com>
2022-05-11 21:52:53 +0900

    Define root state and dispatch types

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import 'tailwindcss/tailwind.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import { Provider } from 'react-redux';
-import store from '@/store/index';
+import { store } from '@/store/index';
 import './_app.scss';
 import AppContainer from '@/layouts/AppContainer';
 
--- store/index.ts
+++ store/index.ts
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from './todoSlice';
 
-export default configureStore({
+export const store = configureStore({
   reducer: {
     todo: todoReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
