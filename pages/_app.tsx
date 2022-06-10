d3a5572b2768983177d3a9c254d71bd19caa3204
Hyeonjong <nianelo4@gmail.com>
2022-06-11 00:41:46 +0900

    Use built-in type for custom 'App'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import type { AppProps } from 'next/app';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'tailwindcss/tailwind.css';
 import 'react-datepicker/dist/react-datepicker.css';
@@ -6,7 +7,7 @@ import { store } from '@/store/index';
 import './_app.scss';
 import AppContainer from '@/layouts/AppContainer';
 
-function MyToDoApp({ Component, pageProps }) {
+function MyToDoApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <AppContainer>
