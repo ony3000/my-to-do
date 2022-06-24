bb91759d89943c4311400f0ed0dea9d40fbbf3b7
Hyeonjong <nianelo4@gmail.com>
2022-06-25 00:09:42 +0900

    Add condition to render layout of application

--- layouts/AppContainer.tsx
+++ layouts/AppContainer.tsx
@@ -73,7 +73,7 @@ export default function AppContainer({ children }: AppContainerProps) {
         { [`is-${settingsPerPage?.themeColor}-theme`]: settingsPerPage?.themeColor },
       )}
     >
-      {isAppReady ? (
+      {isAppReady && pageKey !== '/' ? (
         <>
           <AppHeader />
 
