e1ea68472aef395be9e14757f479c9dbc2be08c9
Hyeonjong <nianelo4@gmail.com>
2023-01-09 00:00:05 +0900

    Update directory configurations

--- next.config.js
+++ next.config.js
@@ -1,19 +1,3 @@
 module.exports = {
   swcMinify: true,
-  eslint: {
-    dirs: [
-      // default directories
-      'pages',
-      'components',
-      'lib',
-      'src',
-
-      // custom directories
-      'hooks',
-      'layouts',
-      'plugins',
-      'store',
-      'types',
-    ],
-  },
 };
