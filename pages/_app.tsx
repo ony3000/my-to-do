db899e43e3c1722c1ade625331824d53c2127d37
Hyeonjong <nianelo4@gmail.com>
2023-01-09 00:04:30 +0900

    Move styles directory

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css';
+import '@/lib/styles/globals.css';
 import type { AppProps } from 'next/app';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'react-datepicker/dist/react-datepicker.css';
